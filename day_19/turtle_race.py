395656e1d0d20dad5bf29aea48f1b51dddeddd00
DevOM <oplondhe@gmail.com>
2022-02-20 18:30:59 +0530

    After completing turtle race project, day 19

--- day_19/turtle_race.py
+++ day_19/turtle_race.py
@@ -1,19 +1,39 @@
+from random import randint
 from turtle import Turtle, Screen
 
 screen = Screen()
 screen.setup(width=500, height=400)
 
-bet = screen.textinput(title="Make your bet",
-                       prompt="Who will win? (red, green, blue, yellow, black)")
+bet = screen.textinput(
+    title="Make your bet",
+    prompt="Who will win? (red, green, blue, yellow, black)"
+)
+colors = ("red", "green", "orange", "blue", "yellow", "purple")
+y_position = (-70, -40, -10, 20, 50, 80)
 
-red = Turtle("turtle")
-green = Turtle("turtle")
-blue = Turtle("turtle")
-yellow = Turtle("turtle")
-black = Turtle("turtle")
+turtles = []
+for turtle_index in range(6):
+    turtle = Turtle(shape="turtle")
+    turtle.penup()
+    turtle.color(colors[turtle_index])
+    turtle.goto(x = -240, y = y_position[turtle_index])
+    turtles.append(turtle)
 
-for turtle in (red, green, blue, yellow, black):
-    turtle.color(str(turtle))
+is_race_on = False
+if bet != "":
+    is_race_on = True
+
+while is_race_on:
+    for turtle in turtles:
+        turtle.pendown()
+        turtle.pensize(1)
+        distance = randint(0, 11)
+        turtle.forward(distance)
+        if turtle.xcor() > 240:
+            turtle_color = turtle.pencolor()
+            print(f"{turtle_color.title()} Turtle wont the race.")
+            print(f"This means, {'won' if bet == turtle_color else 'lost'} the bet!")
+            is_race_on = False
 
 
 screen.exitonclick()
